a46ab0c6c62c20b2fa06477100cc721988ea4455
Eminarti Sianturi <eminartiys@gmail.com>
2017-07-19 18:02:52 +0700

    add layout

---
+++ app/src/main/java/com/application/library/kotlin/ui/book/BookActivity.kt
@@ -0,0 +1,14 @@
+package com.application.library.kotlin.ui.book
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.application.library.kotlin.R
+
+class BookActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_book)
+    }
+}
